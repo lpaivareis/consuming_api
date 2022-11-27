e0cada602143de6290c9d3a040f8d63fe51b4a18
lpaivareis <lucasfelipedepaiva2@gmail.com>
2022-11-27 16:47:48 -0300

    add show view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+
+  def format_remuneration(value, value_period)
+    return "N/A" if value.nil? || value_period.nil?
+
+    "#{value}/#{value_period}"
+  end
+
+  def format_value_description(job)
+    return "" if job.blank?
+
+    "#{format_remuneration(job.value, job.value_period)} • #{job.clinic.name}"
+  end
+
+  def format_city(clinic)
+    return "" if clinic.blank?
+
+    "#{clinic.city}/#{clinic.state}"
+  end
 end
