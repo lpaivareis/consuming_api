dbe82c468bdd28476cf8a2e2c1efb4446aec8ded
lpaivareis <lucasfelipedepaiva2@gmail.com>
2022-11-27 18:07:51 -0300

    fix devise

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,9 +10,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    super
+
+    redirect_to root_path
+  end
 
   # GET /resource/edit
   # def edit
